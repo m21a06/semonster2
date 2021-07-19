c8cf31d9630162da593c07b980d68989ad194c92
sosos-618 <fdaldut1212@gmail.com>
2021-07-19 10:44:05 +0900

    souki semonster2

--- app/src/main/java/semonster2/semonster2.java
+++ app/src/main/java/semonster2/semonster2.java
@@ -1,5 +1,22 @@
 package semonster2;
 
 public class semonster2 {
-  
+  private String name;
+  private int rare; // 戦うとレア度が高いほうが勝つ．同じ場合は引き分け
+
+  semonster2(int nameNum, int rareNum) {
+    this.name = this.summonMonster(nameNum);
+    this.rare = rareNum;
+  }
+
+  String summonMonster(int mnumber) {
+    String monsters[] = { "スライム", "サハギン", "ドラゴン", "デュラハン", "シーサーペント" };
+    return monsters[mnumber];
+  }
+
+  @Override
+  public String toString() {
+
+    return this.name + ":レア度[" + this.rare + "]\n";
+  }
 }
---
+++ app/src/test/java/semonster2/semonser2_Test.java
@@ -0,0 +1,12 @@
+package semonster2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class semonser2_Test {
+  @Test
+  public void testsemonster2() {
+    App classUnderTest = new semonster2();
+    assertNotNull("ドラゴン", classUnderTest.getGreeting());
+  }
+}
